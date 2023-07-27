9f9bafecfbc10541f3c49418e4bc9795628081e7
andrewfurey21 <andrewfurey2003@gmail.com>
2023-07-27 20:14:20 +0100

    started queues

---
+++ queue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Mainline {
+    public static void Main() {
+        Queue<int> q = new Queue<int>();
+        for (int i = 0; i < 1000; i++) {
+            q.Enqueue(i);
+        }
+        for (int i = 0; i < 1000; i++) {
+            Debug.Assert(q.Dequeue() == i);
+        }
+
+        //TODO: fix
+        int a = q.Dequeue();
+        q.Dequeue();
+        q.Dequeue();
+        Console.WriteLine(a);
+    }
+}
---
+++ queue/Queue.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Queue<T> {
+    private class Node {
+        public T data;
+        public Node next;
+    }
+    private int size = 0;
+    public int Size {
+        get {
+            return this.size;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+
+    public Queue() {
+        tail = new Node();
+        head = tail;
+    }
+
+    public T Dequeue() {
+        T data = head.data;
+        if (size > 0) {
+            size--;
+            head = head.next;
+
+        };
+        return data;
+    }
+
+    public void Enqueue(T data) {
+        size++;
+        tail.data = data;
+        tail.next = new Node();
+        tail = tail.next;
+    }
+}
