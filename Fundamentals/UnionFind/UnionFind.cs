4db2d559e4ffc3420ef18de84399d4cda408bdb5
andrewfurey21 <andrewfurey2003@gmail.com>
2024-01-02 13:47:52 +0000

    fleshed out union find class

---
+++ Fundamentals/UnionFind/UnionFind.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+
+namespace Fundamentals;
+public class UnionFind {
+    public UnionFind(int numberOfSites) {
+
+    }
+
+
+    // Add a connection between p and q
+    public void Union(int p, int q) {
+
+    }
+
+    //Component identifier for p
+    public int Find(int p) {
+        return default(int);
+    }
+
+    //Check if p and q are connected
+    public Boolean Connected(int p, int q) {
+        return default(Boolean);
+    }
+
+    //Find number of components
+    public int Count() {
+        return default(int);
+    }
+}
