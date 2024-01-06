4695c3cccca760ac281bcda5e249acfbbaa763d7
andrewfurey21 <andrewfurey2003@gmail.com>
2024-01-06 22:42:43 +0000

    removed change and extra test

--- Sorting/ShellSort/ShellSort.cs
+++ Sorting/ShellSort/ShellSort.cs
@@ -1,8 +1,7 @@
 namespace Sorting;
 public class ShellSort {
-    public static int[] Sort(int[] arr, int h, int change) {
-        //TODO:make change a variable ie k-=change
-        for (int k = h; k >= 1; k-=change) {
+    public static int[] Sort(int[] arr, int h) {
+        for (int k = h; k >= 1; k-=1) {
             for (int i = 0; i < arr.Length; i++) {
                 for (int j = i; j >= 0; j--) {
                     if (j+k < arr.Length && arr[j+k] < arr[j]) {
