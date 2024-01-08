1bca479a0b6b48c49e07bc00947c301ecddbe917
andrewfurey21 <andrewfurey2003@gmail.com>
2024-01-08 20:52:56 +0000

    finished sort method

--- Sorting/QuickSort/QuickSort.cs
+++ Sorting/QuickSort/QuickSort.cs
@@ -3,4 +3,18 @@ public class QuickSort {
     public static int[] Sort(int[] arr) {
         return arr;
     }
+
+    public static int[] Sort(int[] arr, int lowIndex, int highIndex) {
+        if (lowIndex >= highIndex) {
+            return arr;
+        }
+        int pivot = Partition(arr, lowIndex, highIndex);
+        Sort(arr, lowIndex, pivot-1);
+        Sort(arr, pivot+1, highIndex);
+        return arr;
+    }
+
+    public static int Partition(int[] arr, int lowIndex, int highIndex) {
+        return 0;
+    }
 }
